fdfab8731a318791d68354b7674618005401a7db
vir214 <naebal.lohov.eazy@gmail.com>
2025-09-17 17:39:08 +0300

    sssss

--- src/main/java/org/ecore/commands/impl/BanCommand.java
+++ src/main/java/org/ecore/commands/impl/BanCommand.java
@@ -1,5 +1,6 @@
 package org.ecore.commands.impl;
 
+import arc.struct.ObjectMap;
 import arc.util.Strings;
 import arc.util.Time;
 import mindustry.Vars;
@@ -8,17 +9,19 @@ import org.ecore.commands.AbstractCommand;
 import org.ecore.commands.CommandException;
 import org.ecore.database.Cache;
 import org.ecore.database.PlayerData;
+import org.ecore.discord.Webhooks;
+import org.ecore.usefull.Utils;
 
 public class BanCommand extends AbstractCommand {
     public BanCommand(){
-        super("ban", "ban player", "<iporuuid> <days>");
+        super("ban", "ban player", "<iporuuid> <time>");
     }
 
     @Override
     public void run(Player player, String[] args) throws CommandException {
         String text = args[0];
         if (!Strings.canParseInt(args[1])) throw new CommandException("2nd arg must be int");
-        long unbanTimestamp = Time.millis() + Integer.parseInt(args[1]) * 24 * 3600 * 1000L;
+        long unbanTimestamp = Time.millis() + Utils.toMillis(args[1]);
         PlayerData data;
         if (text.length() == 24 && text.endsWith("==")) {
             data = Cache.forceGet(text);
@@ -32,6 +35,12 @@ public class BanCommand extends AbstractCommand {
         data.banned = true;
         data.banneduntil = unbanTimestamp;
         data.save();
+        Webhooks.bans.sendEmbed("Ban", 14177041, ObjectMap.of(
+                "UUID", data.uuid,
+                "ID", data.id,
+                "Admin", player.plainName(),
+                "Unban time", Utils.formatTime(unbanTimestamp)
+        ));
         player.sendMessage("successfully banned " + data.id);
     }
 }
--- src/main/java/org/ecore/usefull/Utils.java
+++ src/main/java/org/ecore/usefull/Utils.java
@@ -1,5 +1,33 @@
 package org.ecore.usefull;
 
-public class Utils {
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class Utils {
+    private static final DateTimeFormatter pattern = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
+    private static final Pattern timePattern = Pattern.compile("^(\\d+)([yYdDhHmMsS])$");
+    public static String formatTime(long millis) {
+        return Instant.ofEpochMilli(millis)
+                .atZone(ZoneId.systemDefault())
+                .toLocalDateTime()
+                .format(pattern);
+    }
+    public static long toMillis(String input) {
+        Matcher matcher = timePattern.matcher(input);
+        if (!matcher.matches()) {
+            return -1;
+        }
+        long value = Long.parseLong(matcher.group(1));
+        return switch (matcher.group(2).toLowerCase()) {
+            case "y", "л" -> value * 365L * 24 * 60 * 60;
+            case "d", "д" -> value * 24L * 60 * 60;
+            case "h", "ч" -> value * 60L * 60;
+            case "m", "м" -> value * 60L;
+            case "s", "с" -> value;
+            default -> -1;
+        } * 1000;
+    }
 }
