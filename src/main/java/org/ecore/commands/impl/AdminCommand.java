1dc6cfaeb6490acef9ffb50792e91ebaf1b85f69
vir214 <naebal.lohov.eazy@gmail.com>
2025-09-17 16:17:10 +0300

    ssssss

--- src/main/java/org/ecore/commands/impl/AdminCommand.java
+++ src/main/java/org/ecore/commands/impl/AdminCommand.java
@@ -1,5 +1,8 @@
 package org.ecore.commands.impl;
 
+import arc.util.Strings;
+import mindustry.Vars;
+import mindustry.gen.Groups;
 import mindustry.gen.Player;
 import org.ecore.commands.AbstractCommand;
 import org.ecore.commands.CommandException;
@@ -15,7 +18,27 @@ public class AdminCommand extends AbstractCommand {
     public void run(Player player, String[] args) throws CommandException {
         String text = args[0];
         if (text.length() == 24 && text.endsWith("==")){
-
+            var sdata = Vars.netServer.admins.getInfo(text);
+            if (sdata == null) throw new CommandException("Cant find this player");
+            boolean admin = sdata.admin;
+            if (admin) {
+                Vars.netServer.admins.unAdminPlayer(text);
+            } else Vars.netServer.admins.adminPlayer(text, sdata.adminUsid);
+            var target = Groups.player.find(p -> p.uuid().equals(text));
+            if (target != null) {
+                player.admin(!admin);
+            }
+        } else {
+            var target = Groups.player.find(p -> p.plainName().equalsIgnoreCase(Strings.stripColors(text)));
+            if (target == null)throw new CommandException("Cant find this player");
+            if (target.admin) {
+                Vars.netServer.admins.unAdminPlayer(target.uuid());
+                target.admin(false);
+            } else {
+                Vars.netServer.admins.adminPlayer(target.uuid(), target.usid());
+                target.admin(true);
+            }
         }
+        player.sendMessage("Done.");
     }
 }
--- src/main/java/org/ecore/database/Database.java
+++ src/main/java/org/ecore/database/Database.java
@@ -16,84 +16,86 @@ public class Database {
 
     @SneakyThrows(SQLException.class)
     public void create()  {
-        Statement statement = con.createStatement();
-        statement.execute("""
-                CREATE TABLE IF NOT EXISTS players(
-                    'id' INTEGER PRIMARY KEY AUTOINCREMENT,
-                    'uuid' VARCHAR(24),
-                    'blockplaced' INTEGER,
-                    'blockbroken' INTEGER,
-                    'timeplayed' INTEGER,
-                    'messagesent' INTEGER,
-                    'banneduntil' INTEGER,
-                    'banned' BOOLEAN,
-                    'rank' VARCHAR(10)
-                )""");
+        try(Statement statement = con.createStatement()) {
+            statement.execute("""
+                    CREATE TABLE IF NOT EXISTS players(
+                        'id' INTEGER PRIMARY KEY AUTOINCREMENT,
+                        'uuid' VARCHAR(24),
+                        'blockplaced' INTEGER,
+                        'blockbroken' INTEGER,
+                        'timeplayed' INTEGER,
+                        'messagesent' INTEGER,
+                        'banneduntil' INTEGER,
+                        'banned' BOOLEAN,
+                        'rank' VARCHAR(10)
+                    )""");
+        }
     }
     @SneakyThrows(SQLException.class)
     public void add(String uuid) {
-        PreparedStatement st = con.prepareStatement("""
+        try(PreparedStatement st = con.prepareStatement("""
                 INSERT INTO players(uuid,blockplaced,blockbroken,timeplayed,messagesent,banneduntil,banned,rank)
                 VALUES(?,0,0,0,0,0,false,"def")
-                """);
-        st.setString(1, uuid);
-        st.execute();
+                """)) {
+            st.setString(1, uuid);
+            st.execute();
+        }
     }
     @SneakyThrows(SQLException.class)
-    public void add(String uuid, boolean banned, String rank, long... other)  {
-        PreparedStatement st = con.prepareStatement("""
+    public void add(String uuid, boolean banned, String rank, long blockplaced,long blockbroken,long timeplayed,long messagesent,long banneduntil)  {
+        try(PreparedStatement st = con.prepareStatement("""
                 INSERT INTO players(uuid,blockplaced,blockbroken,timeplayed,messagesent,banneduntil,banned,rank)
                 VALUES(?,?,?,?,?,?,?)
-                """);
-        st.setString(1, uuid);
-        st.setLong(2, other[0]);
-        st.setLong(3, other[1]);
-        st.setLong(4, other[2]);
-        st.setLong(5, other[3]);
-        st.setLong(6, other[4]);
-        st.setBoolean(7, banned);
-        st.setString(8, rank);
-        st.execute();
+                """)) {
+                        st.setString(1, uuid);
+            st.setLong(2, blockplaced);
+            st.setLong(3, blockbroken);
+            st.setLong(4, timeplayed);
+            st.setLong(5, messagesent);
+            st.setLong(6, banneduntil);
+            st.setBoolean(7, banned);
+            st.setString(8, rank);
+            st.execute();
+        }
     }
     @SneakyThrows(SQLException.class)
-    public void update(int id, String uuid, boolean banned, String rank, long... other) {
-        PreparedStatement st = con.prepareStatement("""
+    public void update(int id, String uuid, boolean banned, String rank, long blockplaced,long blockbroken,long timeplayed,long messagesent,long banneduntil) {
+        try(PreparedStatement st = con.prepareStatement("""
                 UPDATE players SET uuid=?,blockplaced=?,blockbroken=?,timeplayed=?,messagesent=?,banneduntil=?,banned=?,rank=? WHERE id=?
-                """);
-        st.setString(1, uuid);
-        st.setLong(2, other[0]);
-        st.setLong(3, other[1]);
-        st.setLong(4, other[2]);
-        st.setLong(5, other[3]);
-        st.setLong(6, other[4]);
-        st.setBoolean(7, banned);
-        st.setString(8, rank);
-        st.setInt(9, id);
-        st.execute();
+                """)) {
+            st.setString(1, uuid);
+            st.setLong(2, blockplaced);
+            st.setLong(3, blockbroken);
+            st.setLong(4, timeplayed);
+            st.setLong(5, messagesent);
+            st.setLong(6, banneduntil);
+            st.setBoolean(7, banned);
+            st.setString(8, rank);
+            st.setInt(9, id);
+            st.execute();
+        }
     }
     @SneakyThrows(SQLException.class)
     public void setParam(int id, Object data, Param param) {
-        PreparedStatement statement = con.prepareStatement("""
-                UPDATE players SET ?=? WHERE id=?
-                """);
-        statement.setString(1, param.name());
-        statement.setInt(3, id);
-        if (param == Param.uuid) statement.setString(2, (String) data);
-        else if (param == Param.banned) statement.setBoolean(2, (boolean) data);
-        else statement.setInt(2, (int) data);
-        statement.execute();
+        try(PreparedStatement statement = con.prepareStatement("UPDATE players SET " + param.name() + "=? WHERE id=?")) {
+            statement.setInt(2, id);
+            if (param == Param.uuid) statement.setString(1, (String) data);
+            else if (param == Param.banned) statement.setBoolean(1, (boolean) data);
+            else statement.setInt(2, (int) data);
+            statement.execute();
+        }
     }
     @SneakyThrows(Exception.class)
     private PlayerData get(Param pname, Object val)  {
-       PreparedStatement statement = con.prepareStatement("SELECT * FROM players WHERE ?=?", ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY,ResultSet.CLOSE_CURSORS_AT_COMMIT);
-       statement.setString(1, pname.name());
-       if (val instanceof String s) statement.setString(2, s);
-       else statement.setLong(2, (long) val);
-       var resp = statement.executeQuery();
-       if (!resp.next()) return null;
-       return new PlayerData(resp.getInt("id"), resp.getString("uuid"), resp.getInt("blockplaced"),
-                resp.getInt("blockbroken"), resp.getInt("timeplayed"), resp.getInt("messagesent"),
-                resp.getInt("banneduntil"), resp.getBoolean("banned"), resp.getString("rank"));
+       try(PreparedStatement statement = con.prepareStatement("SELECT * FROM players WHERE " + pname.name() + "=?", ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY,ResultSet.CLOSE_CURSORS_AT_COMMIT)) {
+           if (val instanceof String s) statement.setString(1, s);
+           else statement.setLong(1, (long) val);
+           var resp = statement.executeQuery();
+           if (!resp.next()) return null;
+           return new PlayerData(resp.getInt("id"), resp.getString("uuid"), resp.getInt("blockplaced"),
+                   resp.getInt("blockbroken"), resp.getInt("timeplayed"), resp.getInt("messagesent"),
+                   resp.getInt("banneduntil"), resp.getBoolean("banned"), resp.getString("rank"));
+       }
     }
 
     public PlayerData getByUUID(String uuid) {
